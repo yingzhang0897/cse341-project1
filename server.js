61a8a4104590603452446a05ea14b5d768484b51
“yingzhang0897” <yinganticpc@gmail.com>
2025-05-11 00:58:02 +0800

    mongodb connect

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+
+const mongodb = require('./db/database');
+
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port,() => {console.log(`Running on port ${port}`)});
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port,() => {console.log(`Databse is listening and Node Running on port ${port}`)});
+    }
+});
+
